699efbef9321b2b3d87e5ee11f9b9124675e6f74
M034792 JOSE MARTINEZ <jose.martinez@cetysuniversidad.edu.mx>
2024-04-09 17:31:37 -0700

    Added Home, login, and SignUp screens.

--- app/src/main/java/com/example/wizard/MainActivity.kt
+++ app/src/main/java/com/example/wizard/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.wizard
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loginButton: Button
+    private lateinit var signupButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+
+        // Initialize loginButton after setContentView()
+        loginButton = findViewById(R.id.loginbutton)
+        signupButton = findViewById(R.id.signupbutton)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
+        signupButton.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        // Apply window insets listener
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
